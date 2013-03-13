837af48932d70e7462adae780cceab4b477db9a2
mykingla <kingla_pei@163.com>
2013-03-13 16:25:40 +0800

    Remove self.included in import_file

--- lib/import_file.rb
+++ lib/import_file.rb
@@ -1,38 +1,32 @@
 module ImportFile
 
   module UserMethods
-    def User.included(base)
-      base.send(:include, ClassMethods)
-    end
 
     class FormatError < Exception; end
 
     
-    class ClassMethods
-
-      def User.import(file, role)
-        spreadsheet = open_spreadsheet(file)
-        header = spreadsheet.row(1)
-        (2..spreadsheet.last_row).each do |i|
-          row = Hash[[header, spreadsheet.row(i)].transpose]
-          user = find_by_email(row['email']) || new
-          user.attributes = row.to_hash.slice(*accessible_attributes)
-          user.set_role(role)
-          user.save
-        end
+    def User.import(file, role)
+      spreadsheet = open_spreadsheet(file)
+      header = spreadsheet.row(1)
+      (2..spreadsheet.last_row).each do |i|
+        row = Hash[[header, spreadsheet.row(i)].transpose]
+        user = find_by_email(row['email']) || new
+        user.attributes = row.to_hash.slice(*accessible_attributes)
+        user.set_role(role)
+        user.save
       end
-
-      def User.open_spreadsheet(file)
-        original_filename = file.original_filename
-        case File.extname(original_filename)
-        when ".sxc" then Roo::Openoffice.new(file.path, nil, :ignore)
-        when ".xls" then Roo::Excel.new(file.path, nil, :ignore)
-        when ".xlsx" then Roo::Excelx.new(file.path, nil, :ignore)
-        else raise FormatError.new("Incorrect format #{original_filename}")
-        end
-      end
-
     end
+
+    def User.open_spreadsheet(file)
+      original_filename = file.original_filename
+      case File.extname(original_filename)
+      when ".sxc" then Roo::Openoffice.new(file.path, nil, :ignore)
+      when ".xls" then Roo::Excel.new(file.path, nil, :ignore)
+      when ".xlsx" then Roo::Excelx.new(file.path, nil, :ignore)
+      else raise FormatError.new("Incorrect format #{original_filename}")
+      end
+    end
+    
   end
 
 end
