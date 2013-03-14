76f9494402c43850a4670b18c9d962a7a39d0cdf
Kaid <kaid@kaid.me>
2013-03-15 00:20:36 +0800

    made test pass

--- spec/models/attrs_config_spec.rb
+++ spec/models/attrs_config_spec.rb
@@ -2,8 +2,8 @@ require 'spec_helper'
 
 describe AttrsConfig do
   describe '.get' do
-    subject      {AttrsConfig.get(:test_attrs)}
     let(:fields) {{:string_field => :string, :boolean_field => :boolean}}
+    subject      {AttrsConfig.get(:student)}
     before do
       fields.each do |field, field_type|
         FactoryGirl.create(:attrs_config, :role => :student, :field => field, :field_type => field_type)
