64ac1bc680a9b494899ee629ec674e3431a80831
Kaid <kaid@kaid.me>
2013-03-18 14:35:37 +0800

    added avatar to UserMaker

--- script/makers/user_maker.rb
+++ script/makers/user_maker.rb
@@ -2,12 +2,14 @@ require 'bundler'
 Bundler.require(:examples)
 
 class UserMaker
+  AVATAR_DIR = File.join(Rails.root, 'tmp', 'avatars')
+
   attr_reader :type, :real_names, :progressbar
 
 	def initialize(type, real_names)
-		@type        = type.to_sym
+		@type        = type
 		@real_names  = real_names
-    @progressbar = ProgressBar.create title: type.pluralize.capitalize, length: 64, total: real_names.count, format: '%t: |%b>>%i| %p%%'
+    @progressbar = _make_progressbar
 	end
 
 	def self.load_yaml(yaml)
@@ -18,28 +20,44 @@ class UserMaker
 	def produce
 		self.real_names.each_with_index do |real_name, index|
 			num = index + 1
-			user = User.create(user_attrs(real_name, num))
+
+      if File.exists?(_avatar_path(real_name))
+        File.open(_avatar_path(real_name)) do |avatar|
+          User.create(_user_attrs(real_name, num, avatar))
+        end
+      else
+        User.create(_user_attrs(real_name, num))
+      end
+
       self.progressbar.increment
 		end
 	end
 
 private
 
-	def name_template(num)
+  def _avatar_path(name)
+    File.join(AVATAR_DIR, self.type.pluralize, "#{name}.png")
+  end
+
+  def _make_progressbar
+    ProgressBar.create(title:  type.pluralize.capitalize,
+                       length: 64,
+                       total:  real_names.count,
+                       format: '%t: |%b>>%i| %p%%')
+  end
+
+	def _name_template(num)
 		"#{self.type}#{num}"
 	end
 
-	def user_attrs(real_name, num)
+	def _user_attrs(real_name, num, avatar=nil)
 		{
-			login: "#{name_template(num)}", 
-			name: real_name, 
+			login:    "#{_name_template(num)}", 
+			name:     real_name, 
 			password: 1234, 
-			email: "#{name_template(num)}@edu.dev",
-			role: self.type
+			email:    "#{_name_template(num)}@edu.dev",
+			role:     self.type,
+      avatar:   avatar
 		}
-	end
-
-	def have_attached_role?
-		[:student, :teacher].include? self.type
 	end
 end
