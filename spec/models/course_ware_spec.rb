79232004274b1819b6e02dbd1ce5ea795557406d
fushang318 <fushang318@gmail.com>
2013-03-20 15:02:26 +0800

    fix

--- spec/models/course_ware_spec.rb
+++ spec/models/course_ware_spec.rb
@@ -3,6 +3,9 @@ require 'spec_helper'
 describe CourseWare do
 
   describe '关联一个 media_resource' do
+    after(:all) do
+      User.all.each{|user|user.destroy}
+    end
     before(:all) do
       @course_ware = FactoryGirl.create(:course_ware)
       @user = FactoryGirl.create(:user)
