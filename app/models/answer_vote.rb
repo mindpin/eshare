4ddf37ecf259eba3ea47daf47248745c0ceb8934
mykingla <kingla_pei@163.com>
2013-03-27 14:18:38 +0800

    Add up and down in answer_vote model

--- app/models/answer_vote.rb
+++ app/models/answer_vote.rb
@@ -8,6 +8,28 @@ class AnswerVote < ActiveRecord::Base
   validates_uniqueness_of :answer_id, :scope => :creator_id,
                           :message => "每个回答只允许投票一次"
 
+  after_update :update_vote_sum
+
+
+  def update_vote_sum
+    if self.kind_changed
+      self.answer.vote_sum += 1 if self.kind == 'VOTE_UP'
+      self.answer.vote_sum -= 1 if self.kind == 'VOTE_DOWN'
+      self.answer.save
+    end
+  end
+
+
+  def up
+    self.kind = "VOTE_UP"
+    self.save
+  end
+
+
+  def down
+    self.kind = "VOTE_DOWN"
+    self.save
+  end
 
 
   module UserMethods
