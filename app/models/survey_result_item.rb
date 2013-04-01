caa2f7428654bb7342fb0c863b52a92f11f8b00f
fushang318 <fushang318@gmail.com>
2013-04-01 16:22:28 +0800

    提交调查

--- app/models/survey_result_item.rb
+++ app/models/survey_result_item.rb
@@ -2,7 +2,54 @@ class SurveyResultItem < ActiveRecord::Base
   attr_accessible :answer
 
   belongs_to :survey_result
+  belongs_to :survey_item
 
-  validates :survey_result, :answer_choice_mask, :answer_fill, :answer_text,
-            :presence => true
+  validates :survey_result, :survey_item, :presence => true
+
+  validates :answer_choice_mask, :presence => {:if=> lambda {|item|
+    !item.survey_item.blank? && (
+      item.survey_item.kind == SurveyItem::Kind::SINGLE_CHOICE ||
+        item.survey_item.kind == SurveyItem::Kind::MULTIPLE_CHOICE
+      )
+  }}
+
+  validates :answer_fill, :presence => {:if=> lambda {|item|
+    !item.survey_item.blank? && item.survey_item.kind == SurveyItem::Kind::FILL
+  }}
+
+  validates :answer_text, :presence => {:if=> lambda {|item|
+    !item.survey_item.blank? && item.survey_item.kind == SurveyItem::Kind::TEXT
+  }}
+
+  before_validation :set_answer_value
+  def set_answer_value
+    return if self.survey_item.blank?
+
+    case self.survey_item.kind
+    when SurveyItem::Kind::SINGLE_CHOICE, SurveyItem::Kind::MULTIPLE_CHOICE
+      self.answer_choice = @answer
+    when SurveyItem::Kind::TEXT
+      self.answer_text = @answer
+    when SurveyItem::Kind::FILL
+      self.answer_fill = @answer
+    end
+  end
+
+  def answer=(answer)
+    @answer = answer
+  end
+
+  def answer_choice=(choices)
+    value = choices.upcase.split('').map do |choice|
+     2 ** ('A'..'Z').to_a.index(choice.to_s.upcase)
+    end.sum
+    write_attribute(:answer_choice_mask, value)
+  end
+
+  def answer_choice
+    answer_choice_mask &&
+    answer_choice_mask.to_s(2).chars.reverse.each_with_index.inject([]) do |acc, (num,index)|
+      num == "1" ? acc + [('A'..'Z').to_a[index]] : acc
+    end.compact.sort.join('')
+  end
 end
--- spec/models/survey_spec.rb
+++ spec/models/survey_spec.rb
@@ -78,5 +78,82 @@ describe Survey do
         @survey_item.kind.should == SurveyItem::Kind::TEXT
       }
     end
+
+    describe '增加多个问题，在进行回答' do
+      before{
+        @survey_item_1 = @survey.survey_items.create(:kind => SurveyItem::Kind::SINGLE_CHOICE, :content => '你的性别?', :choice_options => ['男','女'] )
+        @survey_item_2 = @survey.survey_items.create(:kind => SurveyItem::Kind::MULTIPLE_CHOICE, :content => '你喜欢的水果?', :choice_options => ['苹果','橘子','梨','其他'])
+        @survey_item_3 = @survey.survey_items.create(:kind => SurveyItem::Kind::FILL, :content => '你的姓名*，你的电话*')
+        @survey_item_4 = @survey.survey_items.create(:kind => SurveyItem::Kind::TEXT, :content => '你有什么建议么？')
+
+        @survey_item_1.id.blank?.should == false
+        @survey_item_2.id.blank?.should == false
+        @survey_item_3.id.blank?.should == false
+        @survey_item_4.id.blank?.should == false
+
+        @survey_result = @survey.survey_results.build
+        @survey_result.creator = @user
+        @survey_result.save
+      }
+
+      it{
+        @survey_result.id.blank?.should == false 
+      }
+
+      it{
+        @survey.survey_results.count.should == 1
+      }
+
+      context '回答第一个问题' do
+        before{
+          @result_item_1 = @survey_result.survey_result_items.build
+          @result_item_1.answer = 'A'
+          @result_item_1.survey_item = @survey_item_1
+          @result_item_1.save
+        }
+
+        it{
+          @result_item_1.answer_choice_mask.should == 1
+        }
+
+        context '回答第二个问题' do
+          before{
+            @result_item_2 = @survey_result.survey_result_items.build
+            @result_item_2.answer = 'AC'
+            @result_item_2.survey_item = @survey_item_2
+            @result_item_2.save
+          }
+
+          it{
+            @result_item_2.answer_choice_mask.should == 5
+          }
+          context '回答第三个问题' do
+            before{
+              @result_item_3 = @survey_result.survey_result_items.build
+              @result_item_3.answer = 'fushang318,110'
+              @result_item_3.survey_item = @survey_item_3
+              @result_item_3.save
+            }
+
+            it{
+              @result_item_3.answer_fill.should == 'fushang318,110'
+            }
+            context '回答第四个问题' do
+              before{
+                @result_item_4 = @survey_result.survey_result_items.build
+                @result_item_4.answer = '没有建议，一切都好'
+                @result_item_4.survey_item = @survey_item_4
+                @result_item_4.save
+              }
+
+              it{
+                @result_item_4.answer_text.should == '没有建议，一切都好'
+              }
+            end
+          end
+        end
+      end
+
+    end
   end
 end
