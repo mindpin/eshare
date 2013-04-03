cc9afcc5d4b91af46328602cc34b73b0d2349b45
mykingla <kingla_pei@163.com>
2013-04-03 18:02:28 +0800

    category rspec

--- spec/models/category_spec.rb
+++ spec/models/category_spec.rb
@@ -22,14 +22,20 @@ describe Category do
       @root_nodes.should == ["microsoft", "Linux"]
     end
 
-    it "第二级目录" do
+    it "第二级 microsoft 目录" do
       @nodes[@microsoft].keys.should == ['xp', 'vista', 'windows8']
+    end
+
+    it "第二级 linux 目录" do
       @nodes[@linux].keys.should == ['slackware', 'debian']
     end
 
-    it "第三级目录" do
-      @nodes[@linux][@slackware].keys.should == ['test', 'alpha']
+    it "第三级 debian 目录" do
       @nodes[@linux][@debian].keys.should == ['etch', 'squeeze']
+    end
+
+    it "第三级 slackware 目录" do
+      @nodes[@linux][@slackware].keys.should == ['test', 'alpha']
     end
   end
 
