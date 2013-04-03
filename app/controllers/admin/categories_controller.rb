b5b1a898b364048215981b8c2be59ae0bc17d0e8
mykingla <kingla_pei@163.com>
2013-04-03 10:43:56 +0800

    Implement crud of categories

--- app/controllers/admin/categories_controller.rb
+++ app/controllers/admin/categories_controller.rb
@@ -7,19 +7,33 @@ class Admin::CategoriesController < ApplicationController
     @category = Category.find(params[:id]) if params[:id]
   end
 
+  def index
+    @categories = Category.all
+  end
+
   def new
     @category = Category.new
+
   end
 
   def edit
   end
 
   def create
+    @category = Category.new(params[:category])
+    if @category.save
+      redirect_to "/admin/categories"
+    end
   end
 
   def update
+    if @category.update_attributes(params[:category])
+      redirect_to "/admin/categories"
+    end
   end
 
   def destroy
+    @category.destroy
+    redirect_to :back
   end
 end
--- app/models/category.rb
+++ app/models/category.rb
@@ -1,3 +1,6 @@
 class Category < ActiveRecord::Base
+  attr_accessible :name, :parent_id
+
   acts_as_nested_set
+
 end
