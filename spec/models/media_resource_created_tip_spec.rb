8bdae5f071ff08aeefd2225143d5c81dfd849b07
fushang318 <fushang318@gmail.com>
2013-04-03 14:52:03 +0800

    完成 个人网盘文件的创建后提示

--- spec/models/media_resource_created_tip_spec.rb
+++ spec/models/media_resource_created_tip_spec.rb
@@ -8,15 +8,15 @@ describe '根据个人网盘中文件名进行一些提示' do
     }
 
     it{
-      @media_resource.create_tips.count.should == 1
+      @media_resource.created_tips.count.should == 1
     }
 
     it{
-      @media_resource.create_tips.first.kind.should == MediaResourceCreatedTip::MATCH_CATEGORY_NAME
+      @media_resource.created_tips.first.kind.should == MediaResourceCreatedTip::MATCH_CATEGORY_NAME
     }
 
     it{
-      @media_resource.create_tips.first.info[:category_ids].should == [@category.id]
+      @media_resource.created_tips.first.info[:categories].should =~ [@category]
     }
   end
 
@@ -26,19 +26,19 @@ describe '根据个人网盘中文件名进行一些提示' do
       @media_resource_1 = FactoryGirl.create(:media_resource, :file, :name => "java.ppt", :category => @category)
       @media_resource_2 = FactoryGirl.create(:media_resource, :file, :name => "java.ppt")
 
-      @media_resource = FactoryGirl.create(:media_resource, :file, :name => "java.ppt")
+      @media_resource = FactoryGirl.create(:media_resource, :file, :name => "java.PPT")
     }
 
     it{
-      @media_resource.create_tips.count.should == 1
+      @media_resource.created_tips.count.should == 1
     }
 
     it{
-      @media_resource.create_tips.first.kind.should == MediaResourceCreatedTip::MATCH_PUBLIC_RESOURCE_NAME
+      @media_resource.created_tips.first.kind.should == MediaResourceCreatedTip::MATCH_PUBLIC_RESOURCE_NAME
     }
 
     it{
-      @media_resource.create_tips.first.info[:media_resource_ids].should == [@media_resource_1.id]
+      @media_resource.created_tips.first.info[:media_resources].should =~ [@media_resource_1]
     }
 
   end
@@ -49,11 +49,11 @@ describe '根据个人网盘中文件名进行一些提示' do
     }
 
     it{
-      @media_resource.create_tips.count.should == 1 
+      @media_resource.created_tips.count.should == 1 
     }
 
     it{
-      @media_resource.create_tips.first.kind.should == MediaResourceCreatedTip::MATCH_NOTHING
+      @media_resource.created_tips.first.kind.should == MediaResourceCreatedTip::MATCH_NOTHING
     }
   end
 
@@ -72,11 +72,11 @@ describe '根据个人网盘中文件名进行一些提示' do
       }
 
       it{
-        @media_resource_1.create_tips.count.should == 1 
+        @media_resource_1.created_tips.count.should == 1 
       }
 
       it{
-        @media_resource_1.create_tips.first.kind.should == MediaResourceCreatedTip::MATCH_NOTHING
+        @media_resource_1.created_tips.first.kind.should == MediaResourceCreatedTip::MATCH_NOTHING
       }
 
       context '再上传一个文件 名字是 梨' do
@@ -85,11 +85,11 @@ describe '根据个人网盘中文件名进行一些提示' do
         }
 
         it{
-          @media_resource_li.create_tips.count.should == 1 
+          @media_resource_li.created_tips.count.should == 1 
         }
 
         it{
-          @media_resource_li.create_tips.first.kind.should == MediaResourceCreatedTip::MATCH_NOTHING
+          @media_resource_li.created_tips.first.kind.should == MediaResourceCreatedTip::MATCH_NOTHING
         }
       end
 
@@ -99,15 +99,15 @@ describe '根据个人网盘中文件名进行一些提示' do
         }
 
         it{
-          @media_resource_2.create_tips.count.should == 1 
+          @media_resource_2.created_tips.count.should == 1 
         }
 
         it{
-          @media_resource_2.create_tips.first.kind.should == MediaResourceCreatedTip::MATCH_CATEGORY_NAME
+          @media_resource_2.created_tips.first.kind.should == MediaResourceCreatedTip::MATCH_CATEGORY_NAME
         }
 
         it{
-          @media_resource_2.create_tips.first.info[:category_ids].should =~ [@category_shouji_pingguo.id, @category_shuiguo_pingguo.id]
+          @media_resource_2.created_tips.first.info[:categories].should =~ [@category_shouji_pingguo, @category_shuiguo_pingguo]
         }
 
         context '共享上一个文件，上传一个新文件 名字是苹果' do
@@ -118,17 +118,23 @@ describe '根据个人网盘中文件名进行一些提示' do
           }
 
           it{
-            @media_resource_3.create_tips.map(&:kind) =~ [MediaResourceCreatedTip::MATCH_CATEGORY_NAME, MediaResourceCreatedTip::MATCH_PUBLIC_RESOURCE_NAME]
+            @media_resource_3.created_tips.map(&:kind) =~ [MediaResourceCreatedTip::MATCH_CATEGORY_NAME, MediaResourceCreatedTip::MATCH_PUBLIC_RESOURCE_NAME]
           }
 
           it{
-            tip = @media_resource_3.create_tips.select{|tip|tip.kind == MediaResourceCreatedTip::MATCH_CATEGORY_NAME}[0]
-            tip.info[:category_ids].should =~ [@category_shouji_pingguo.id, @category_shuiguo_pingguo.id]
+            tip = @media_resource_3.created_tips.select{|tip|tip.kind == MediaResourceCreatedTip::MATCH_CATEGORY_NAME}[0]
+            tip.info[:categories].should =~ [@category_shouji_pingguo, @category_shuiguo_pingguo]
           }
 
           it{
-            tip = @media_resource_3.create_tips.select{|tip|tip.kind == MediaResourceCreatedTip::MATCH_PUBLIC_RESOURCE_NAME}[0]
-            tip.info[:media_resource_ids].should =~ [@media_resource_2.id]
+            tip = @media_resource_3.created_tips.select{|tip|tip.kind == MediaResourceCreatedTip::MATCH_PUBLIC_RESOURCE_NAME}[0]
+            tip.info[:media_resources].should =~ [@media_resource_2]
+          }
+
+          it{
+            @media_resource_3.to_public(@category_shuiguo)
+            tip = @media_resource_3.created_tips.select{|tip|tip.kind == MediaResourceCreatedTip::MATCH_PUBLIC_RESOURCE_NAME}[0]
+            tip.info[:media_resources].should =~ [@media_resource_2]
           }
         end
       end
