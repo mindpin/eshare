115820cf75bf32f84058c4d2f1b4f385b14461db
mykingla <kingla_pei@163.com>
2013-04-08 14:03:36 +0800

    fix

--- spec/models/question_follow_spec.rb
+++ spec/models/question_follow_spec.rb
@@ -1,40 +1,35 @@
 require "spec_helper"
 
 describe QuestionFollow do
-  before {
-    @creator      = FactoryGirl.create :user
-    @question     = FactoryGirl.create(:question, :creator => @creator)
+  
 
-  }
 
   describe "问题创建者" do 
+    before {
+      @creator      = FactoryGirl.create :user
+      @question     = FactoryGirl.create(:question, :creator => @creator)
+    }
 
-    context "问题有被自己关注" do
-      it {
-        @question.follow_by_user(@creator).persisted?.should == true
-      }
-
-      it {
-        @question.followed_by?(@creator).should == true
-      }
+    it "问题有被自己关注" do
+      @question.follow_by_user(@creator).persisted?.should == true
+ 
+      @question.followed_by?(@creator).should == true
     end
 
 
-    context "取消关注" do
-      it "关注数量为0" do
-        expect {
-          @creator.unfollow_question(@question)
-        }.to change {
-          @question.follows.by_user(@creator).count
-        }.by(-1)
-      end
-
-
-      it "已经被取消" do
+    it "关注数量为0" do
+      expect {
         @creator.unfollow_question(@question)
+      }.to change {
+        @question.follows.by_user(@creator).count
+      }.by(-1)
+    end
 
-        @question.followed_by?(@creator).should == false
-      end
+
+    it "已经被取消" do
+      @creator.unfollow_question(@question)
+
+      @question.followed_by?(@creator).should == false
     end
 
   end
@@ -45,35 +40,33 @@ describe QuestionFollow do
 
     before {
       @user         = FactoryGirl.create :user
+      @question     = FactoryGirl.create(:question)
     }
 
-    context "问题没被其它用户关注" do
-      it {
-        @question.follow_by_user(@user).new_record?.should == true
-      }
+    it {
+      @question.follow_by_user(@user).new_record?.should == true
+    }
 
-      it {
-        @question.followed_by?(@user).should == false
-      }
-    end
+    it {
+      @question.followed_by?(@user).should == false
+    }
 
 
-    context "关注问题" do
-      it "关注数量为1" do
-        expect {
-          @user.follow_question(@question)
-        }.to change {
-          @question.follows.by_user(@user).count
-        }.by(1)
-      end
-
-
-      it "已经被关注" do
+    it "关注数量为1" do
+      expect {
         @user.follow_question(@question)
-
-        @question.followed_by?(@user).should == true
-      end
+      }.to change {
+        @question.follows.by_user(@user).count
+      }.by(1)
     end
+
+
+    it "已经被关注" do
+      @user.follow_question(@question)
+
+      @question.followed_by?(@user).should == true
+    end
+
 
     it "查看记录时间" do
       @user.follow_question(@question)
