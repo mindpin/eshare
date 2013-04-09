9eb214a80ba83a4ea650f57fb42e3c77540b3648
fushang318 <fushang318@gmail.com>
2013-04-09 15:37:01 +0800

    https://github.com/mindpin/eshare/issues/39

--- spec/models/question_notice_spec.rb
+++ spec/models/question_notice_spec.rb
@@ -2,8 +2,8 @@ require 'spec_helper'
 
 describe 'question_notice' do
   before{
-    @user = FactoryGril.create(:user)
-    @question = FactoryGril.create(:question)
+    @user = FactoryGirl.create(:user)
+    @question = FactoryGirl.create(:question)
   }
 
   it{
@@ -12,7 +12,11 @@ describe 'question_notice' do
 
   describe '关注的问题' do
     before{
-      @user.follow_question(@question)
+      @question.follow_by_user(@user)
+    }
+
+    it{
+      @question.followed_by?(@user).should == true
     }
 
     it{
@@ -21,48 +25,47 @@ describe 'question_notice' do
 
     describe '关注的问题发生变化' do
       before{
-        @question.update_attribute(:content => '改')
+        Timecop.travel(Time.now + 1.seconds)
+        @question.update_attributes(:content => '改')
       }
 
       it{
-        @user.notice_questions.should =~ [@question]
+        @user.notice_questions.should == [@question]
       }
 
       it{
-        @user.visit_question!(@question)
+        Timecop.travel(Time.now + 1.seconds)
+        @question.visit_by!(@user)
         @user.notice_questions.should == [] 
       }
 
-      describe '被指定为回答者，没有回答' do
+      describe '被指定为回答者' do
         before{
-          @question_1 = FactoryGril.create(:question, :ask_to => @user)
+          @question_1 = FactoryGirl.create(:question, :ask_to => @user)
         }
 
         it{
-          @user.notice_questions.should =~ [@question]   
+          @user.notice_questions.should == [@question_1, @question]   
         }
 
         it{
-          @question_1.answer.create!(:content => '我是回答', :creator => @user)
-          @user.notice_questions.should =~ [@question_1, @question]   
+          @question_1.answers.create!(:content => '我是回答', :creator => @user)
+          @user.notice_questions.should == [@question]   
         }
 
         it{
-          @question_1.answer.create!(:content => '我是回答', :creator => @user)
-          @user.visit_question!(@question)
-          @user.notice_questions.should =~ [@question_1]
+          @question.visit_by!(@user)
+          @user.notice_questions.should == [@question_1]
         }
 
         it{
-          @question_1.answer.create!(:content => '我是回答', :creator => @user)
-          @user.visit_question!(@question_1)
-          @user.notice_questions.should =~ [@question]
+          @question_1.visit_by!(@user)
+          @user.notice_questions.should == [@question_1, @question]
         }
 
         it{
-          @question_1.answer.create!(:content => '我是回答', :creator => @user)
-          @user.visit_question!(@question_1)
-          @user.visit_question!(@question)
+          @question_1.answers.create!(:content => '我是回答', :creator => @user)
+          @question.visit_by!(@user)
           @user.notice_questions.should == []
         }
 
