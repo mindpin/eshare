4496a039da0c3eed818fe3a8d52cf41d55a7328c
mykingla <kingla_pei@163.com>
2013-04-10 14:40:47 +0800

    Add anonymous rspec for question and answer model

--- spec/factories/question.rb
+++ spec/factories/question.rb
@@ -2,7 +2,7 @@ FactoryGirl.define do
   factory :question do
     sequence(:title){|n| "question_title_#{n}" }
     sequence(:content){|n| "content_#{n}" }
-
+    is_anonymous false
     creator
   end
 end
