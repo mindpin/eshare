aa6c6224bb13bf0b4f3ba9c26d167cb7e0af7be9
fushang318 <fushang318@gmail.com>
2013-04-19 21:07:38 +0800

    change uploader url

--- app/uploaders/image_uploader_methods.rb
+++ app/uploaders/image_uploader_methods.rb
@@ -11,6 +11,14 @@ module ImageUploaderMethods
     File.join(R::UPLOAD_BASE_PATH, "uploads/#{model.class.to_s.underscore}/#{mounted_as}/#{model.id}")
   end
 
+  def url
+    _file_name = model[mounted_as]
+    return default_url if _file_name.blank?
+
+    prefix = version_name.blank? ? '' : "#{version_name}_"
+    File.join('/', R::STATIC_FILES_DIR, "uploads/#{model.class.to_s.underscore}/#{mounted_as}/#{model.id}/#{prefix}#{_file_name}")
+  end
+
   # 允许上传的文件类型的扩展名
   def extension_white_list
     %w(jpg jpeg gif png)
