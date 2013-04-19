3a372702bb6440dd501234333f392dffb0347936
Kaid <kaid@kaid.me>
2013-04-19 11:09:11 +0800

    put sidekiq web in a separate ru

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,5 @@
 # -*- coding: utf-8 -*-
-require 'sidekiq/web'
-
 Eshare::Application.routes.draw do
-  mount Sidekiq::Web => '/sidekiq'
-
   root :to => 'index#index'
   get '/dashboard' => 'index#dashboard'
 
