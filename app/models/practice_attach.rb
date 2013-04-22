88cea064d07e90676c618dfdaec2a98736ea61a9
mykingla <kingla_pei@163.com>
2013-04-22 17:36:31 +0800

    Add file_entity relation on practice_attach model

--- app/models/practice_attach.rb
+++ app/models/practice_attach.rb
@@ -2,6 +2,7 @@ class PracticeAttach < ActiveRecord::Base
   attr_accessible :practice, :file_entity, :name
 
   belongs_to :practice
+  belongs_to :file_entity
 
 
   validates :practice, :file_entity, :name, :presence => true
