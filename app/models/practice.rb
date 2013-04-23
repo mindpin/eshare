7a3d1f2cf5041ba50a35d2c9858e54a5a3a7cb47
mykingla <kingla_pei@163.com>
2013-04-23 11:21:37 +0800

    Add realtion between practice and records

--- app/models/practice.rb
+++ app/models/practice.rb
@@ -8,6 +8,8 @@ class Practice < ActiveRecord::Base
 
   has_many :attaches, :class_name => 'PracticeAttach', :foreign_key => :practice_id
   has_many :requirements, :class_name => 'PracticeRequirement', :foreign_key => :practice_id
+  has_many :records, :class_name => 'PracticeRecord', :foreign_key => :practice_id
+  has_many :uploads, :through => :records
 
   accepts_nested_attributes_for :attaches
   accepts_nested_attributes_for :requirements
--- app/models/practice_requirement.rb
+++ app/models/practice_requirement.rb
@@ -2,6 +2,7 @@ class PracticeRequirement < ActiveRecord::Base
   attr_accessible :practice, :content
 
   belongs_to :practice
+  has_many :uploads, :class_name => 'PracticeUpload', :foreign_key => :requirement_id
 
   validates :content, :presence => true
 
