cd15f7ebba3abf5966b68697e76f5f4665d48e6b
menxu <menxu_work@163.com>
2013-04-23 16:16:38 +0800

    优化

--- app/models/activity_membership.rb
+++ app/models/activity_membership.rb
@@ -7,7 +7,7 @@ class ActivityMembership < ActiveRecord::Base
   validates  :user,     :presence => true
   validates  :activity, :presence => true
 
-  scope :by_activity_and_user, lambda {|activity,user| {:conditions => ['activity_id = ? AND user_id = ?',activity.id,user.id]} }
+  validates  :user_id,  :uniqueness => {:scope => :activity_id}
 
   module ActivityMethods
     def self.included(base)
@@ -18,18 +18,12 @@ class ActivityMembership < ActiveRecord::Base
 
     module InstanceMethods
       def add_member(user)
-        self.activity_memberships.create :user => user if !_by_user(user)
+        self.activity_memberships.create :user => user
       end
 
       def remove_member(user)
-        self.activity_memberships.where(:user_id=>user.id).destroy_all if _by_user(user)
+        self.activity_memberships.where(:user_id => user.id).destroy_all if !user.blank?
       end
-      
-      private
-        def _by_user(user)
-          return if user.blank?
-          ActivityMembership.by_activity_and_user(self,user).first
-        end
     end
   end
 end
