b566f23ce66d5ce530a7e9bfda5e86459f2d7d1b
fushang318 <fushang318@gmail.com>
2013-04-24 14:33:57 +0800

    fix #62

--- app/models/activity.rb
+++ app/models/activity.rb
@@ -1,4 +1,7 @@
 class Activity < ActiveRecord::Base
+  STATUS_PREP = 'PREP'
+  STATUS_BEGIN = 'BEGIN'
+  STATUS_END = 'END'
   attr_accessible :title, :content, :start_time, :end_time, :status
 
   belongs_to :creator, :class_name => 'User', :foreign_key => :creator_id
@@ -6,21 +9,22 @@ class Activity < ActiveRecord::Base
   validates  :creator, :presence => true
   validates  :title,   :presence => true
   validates  :content, :presence => true
-  validates  :status, :inclusion  => %w(PREP BEGIN END)
+  validates  :status, :inclusion  => [STATUS_PREP, STATUS_BEGIN, STATUS_END]
   validates  :start_time, :presence => true
 
   validate   :validate_start_time
-  validate   :validate_end_time
-
   def validate_start_time
     # 这个判断是为了程序健壮考虑
+    return true if start_time.blank?
     if start_time < Time.now
       errors.add(:base,'活动开始时间必须大于现在')
     end
   end
 
+  validate   :validate_end_time
   def validate_end_time
     # 这个判断是为了程序健壮考虑
+    return true if start_time.blank?
     return true if end_time.blank?
     if end_time < Time.now || end_time < start_time
       errors.add(:base,'活动结束时间必须大于开始时间')
@@ -29,25 +33,18 @@ class Activity < ActiveRecord::Base
 
   def end!
     return if !self.end_time.blank?
-    _update_status('END')
+    self.update_attributes :status => STATUS_END
   end
 
   def refresh_status!
     if Time.now < start_time
-      status = 'PREP'
+      self.update_attributes :status => STATUS_PREP
     elsif !end_time.blank? && Time.now < end_time
-      status = 'BEGIN'
+      self.update_attributes :status => STATUS_BEGIN
     elsif !end_time.blank? && Time.now > end_time
-      status = 'END'
+      self.update_attributes :status => STATUS_END
     end
-    _update_status(status)
   end
-
-  private
-    def _update_status(status)
-      self.update_attributes :status => status
-      self.save
-    end
 
   module UserMethods
     def self.included(base)
