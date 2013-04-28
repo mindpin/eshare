a14cad25c171abb1af90d860d125b5bc22557445
mykingla <kingla_pei@163.com>
2013-04-28 19:01:20 +0800

    fix youku import

--- script/youku_import.rb
+++ script/youku_import.rb
@@ -2,14 +2,18 @@ class Youku
 
   def parse_youku_list(url)
     id  = url.match(/^http:\/\/www\.youku\.com\/show_page\/id_(\w*)\.html$/)[1]
+
     doc = Nokogiri::HTML(open(youku_list_url(id)), nil, 'utf-8')
+
     chapters = doc.css('li a').map {|el| [el.attributes['title'].value, el.attributes['href'].value] }
 
-    doc = Nokogiri::HTML(open('http://www.youku.com/show_page/id_zb3df4274140a11e1a046.html'), nil, 'utf-8')
+    doc = Nokogiri::HTML(open(url), nil, 'utf-8')
 
     
     course_desc = doc.css('.aspect_con .detail')[0].content.strip
     course_name = doc.css('.name')[0].content.strip
+
+
 
     {:name => course_name, :desc => course_desc, :chapters => chapters}
   end
@@ -20,16 +24,8 @@ class Youku
 
 end
 
-=begin
-course = Course.find_by_cid(9999999999)
-course.chapters.each do |c|
-  c.course_wares.destroy_all
-  c.destroy
-end
-course.destroy
-=end
 
-# youku_page = 'http://www.youku.com/show_page/id_zb3df4274140a11e1a046.html'
+
 youku_page = ARGV[0]
 
 
@@ -39,15 +35,21 @@ user = User.first
 youku = Youku.new
 data = youku.parse_youku_list(youku_page)
 
+while true
+  r = Random.new
+  cid = r.rand(100...999999999)
+  break if !Course.where(:cid => cid).exists?
+end
 
 
 course = Course.create(
   :name => data[:name],
   :desc => data[:desc],
-  :cid => '9999999999',
+  :cid => cid,
   :creator => user
 )
 
+# p course.errors
 
 data[:chapters].each do |c|
   title = c[0]
