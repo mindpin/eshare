589df7bbf8020592885638b8a0c6184f1a6a3478
ben7th <ben7th@sina.com>
2013-05-15 20:18:13 +0800

    bugfix 弹幕overflow问题 头像路径问题

--- app/uploaders/avatar_uploader.rb
+++ app/uploaders/avatar_uploader.rb
@@ -9,7 +9,7 @@ class AvatarUploader < CarrierWave::Uploader::Base
   def default_url
     # For Rails 3.1+ asset pipeline compatibility:
     # asset_path("fallback/" + [version_name, "default.png"].compact.join('_'))
-    "default_avatars/#{version_name}.png"
+    "/assets/default_avatars/#{version_name}.png"
   end
 
   # 图片裁剪
