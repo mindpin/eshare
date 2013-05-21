a1da538007d58878561caf470fd97aea7b75a882
mykingla <kingla_pei@163.com>
2013-05-21 12:56:07 +0800

    Fix remove of last_view_time

--- db/migrate/20130521032121_remove_default_value_of_last_view_time.rb
+++
@@ -1,6 +0,0 @@
-class RemoveDefaultValueOfLastViewTime < ActiveRecord::Migration
-  def change
-    remove_column :question_follows, :last_view_time
-    add_column :question_follows, :last_view_time, :datetime
-  end
-end
