4513fad363f9b77bbceb559deae1f8e8905d6b1b
menxu <menxu_work@163.com>
2013-05-24 10:28:30 +0800

    update teams methods add has_mang

--- app/models/team.rb
+++ app/models/team.rb
@@ -11,13 +11,12 @@ class Team < ActiveRecord::Base
   module UserMethods
     def self.included(base)
       base.has_many :teams, :foreign_key => :creator_id
+      base.has_many :created_teams, :class_name => 'Team', :foreign_key => :creator_id
     end
 
     # 1.user.teams.create(:name => xxxx)
-    # 4.查询用户创建的 teams
-    def created_teams
-      self.teams
-    end
+    # 4.查询用户创建的 created_teams
+
   end
 
   include TeamMembership::TeamMethods
--- app/models/team_membership.rb
+++ app/models/team_membership.rb
@@ -13,18 +13,15 @@ class TeamMembership < ActiveRecord::Base
   module UserMethods
     def self.included(base)
       base.has_many :user_memberships, :class_name  => 'TeamMembership'
-      base.has_many :joined_team, :through => :user_memberships, :source=> :team
+      base.has_many :joined_teams, :through => :user_memberships, :source=> :team
     end
-    # 5.查询用户参与的 teams(用户不是创建者，而是参与者的 team)
-    def joined_teams
-      self.joined_team
-    end
+    # 5.查询用户参与的 teams(用户不是创建者，而是参与者的 team) joined_teams
   end
 
   module TeamMethods
     def self.included(base)
       base.has_many :team_memberships
-      base.has_many :joined_user, :through => :team_memberships, :source=> :user
+      base.has_many :members, :through => :team_memberships, :source=> :user
     end
     # 2.增加 team 参与者
     def add_member(user)
@@ -34,10 +31,7 @@ class TeamMembership < ActiveRecord::Base
     def remove_member(user)
       self.team_memberships.by_user(user).destroy_all
     end
-    # 6.查询 team 的参与者(不包括创建者)
-    def members
-      self.joined_user
-    end
+    # 6.查询 team 的参与者(不包括创建者) members
   end
 
 
