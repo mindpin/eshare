5c5a2d645c95dc36158e3553d4e373d0a2e559ee
fushang318 <fushang318@gmail.com>
2013-05-27 17:05:46 +0800

    add verify

---
+++ lib/project_verify.rb
@@ -0,0 +1,37 @@
+class ProjectVerify
+  VERIFY_SERVER = 'http://211.101.1.178'
+
+  def check
+    code = _get_key_and_mac
+    url = File.join(VERIFY_SERVER, "/check/#{code}")
+    if Net::HTTP.get_response(URI(url)).code != '200'
+      raise 'verify error'
+    end
+  end
+
+  def get_mac
+    re = %r/(?:[^:\-]|\A)(?:[0-9A-F][0-9A-F][:\-]){5}[0-9A-F][0-9A-F](?:[^:\-]|\Z)/io
+
+    output = `ifconfig`
+    candidates = output.split(/\n/).select{|line| line =~ re}
+    candidates.map{|c| c[re].strip}.first
+  end
+
+  private
+    def ___(key, mac)
+      Digest::MD5.hexdigest("#{key}+#{mac}")
+    end
+
+    def ___path
+      Rails.root.join('public/project.key')
+    end
+
+    def _get_key_and_mac
+      key_path = ___path
+      raise 'verify error' if !File.exists?(key_path)
+      key = File.open(___path).read
+      mac = get_mac
+      ___(key, mac)
+    end
+
+end
---
+++ script/init_verify_key.rb
@@ -0,0 +1,16 @@
+require File.expand_path('../../lib/project_verify', __FILE__)
+require File.expand_path('../../config/initializers/mindpin_global_methods', __FILE__)
+verify = ProjectVerify.new
+
+key_path = File.expand_path('../../public/project.key', __FILE__)
+if File.exists?(key_path)
+  key = File.open(key_path).read
+else
+  key = randstr(16)
+  File.open(key_path,'w') {|f|f << key}
+end
+mac = verify.get_mac
+p "################"
+p "key #{key}"
+p "mac #{mac}"
+p "################"
