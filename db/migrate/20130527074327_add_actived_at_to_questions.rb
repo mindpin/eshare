25d59a6e89325ed05872cd88796acc4ef9694bc8
ben7th <ben7th@sina.com>
2013-05-27 19:05:47 +0800

    update question page

--- db/migrate/20130527074327_add_actived_at_to_questions.rb
+++ db/migrate/20130527074327_add_actived_at_to_questions.rb
@@ -2,7 +2,7 @@ class AddActivedAtToQuestions < ActiveRecord::Migration
   def change
     add_column :questions, :actived_at, :datetime
 
-    ActiveRecord::Base.record_timestamps = false
+    Question.record_timestamps = false
     Question.all.each do |q|
       q.actived_at = q.updated_at
       q.save
