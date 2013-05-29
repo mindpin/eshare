2fc0749535b05e06502a436b5a59e5c62fd9925c
fushang318 <fushang318@gmail.com>
2013-05-29 14:49:19 +0800

    add default_data script

--- app/models/yaml_courses_importer.rb
+++ app/models/yaml_courses_importer.rb
@@ -8,7 +8,19 @@ module YamlCoursesImporter
       progress = SimpleProgress.new parse_yaml.count
       parse_yaml.each do |course_hash|
         hash = HashWithIndifferentAccess.new course_hash
-        user = User.limit(20)[rand 20] || User.first
+        user = User.first
+        CourseZipImporter::Importer.new.init_with_hash(hash, user).import
+        progress.increment
+      end
+    end
+
+    def import_from_yaml_for_web
+      progress = SimpleProgress.new parse_yaml.count
+      count = parse_yaml.count
+      parse_yaml.each_with_index do |course_hash, index|
+        user_id = (index+1) % 19 + 2
+        hash = HashWithIndifferentAccess.new course_hash
+        user = User.find(user_id)
         CourseZipImporter::Importer.new.init_with_hash(hash, user).import
         progress.increment
       end
