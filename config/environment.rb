c17ce7a774ef0c887ce767289014a54ff8e26eb7
mykingla <kingla_pei@163.com>
2013-06-06 10:28:11 +0800

    Analytics words by weibo text

--- config/environment.rb
+++ config/environment.rb
@@ -1,5 +1,11 @@
 # Load the rails application
 require File.expand_path('../application', __FILE__)
 
+
+# 中文分词
+require "rmmseg"
+RMMSeg::Dictionary.load_dictionaries
+
+
 # Initialize the rails application
 Eshare::Application.initialize!
