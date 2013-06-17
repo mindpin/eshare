e3c727fc52bc539f7eae35f9ba2737bc868878e5
mykingla <kingla_pei@163.com>
2013-06-17 14:28:25 +0800

    Initial user_opinion_uploader

---
+++ app/uploaders/user_opinion_uploader.rb
@@ -0,0 +1,23 @@
+class UserOpnionUploader < CarrierWave::Uploader::Base
+  include CarrierWave::MiniMagick
+  include ImageUploaderMethods
+
+  # 存储方式 本地硬盘存储
+  storage :file
+
+  # 当文件不存在时的默认 url
+  def default_url
+    # For Rails 3.1+ asset pipeline compatibility:
+    # asset_path("fallback/" + [version_name, "default.png"].compact.join('_'))
+    "/assets/default_user_opinions/#{version_name}.png"
+  end
+
+  # 图片裁剪
+  version :normal do
+    process :resize_to_fill => [300, 300]
+  end
+
+  version :small do
+    process :resize_to_fill => [100, 100]
+  end
+end
