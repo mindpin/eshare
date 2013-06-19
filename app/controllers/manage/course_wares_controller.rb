9331da9b2635c834eeefa3d872700e84eb49e49d
fushang318 <fushang318@gmail.com>
2013-06-19 15:36:57 +0800

    bug 105 fix

--- app/controllers/manage/course_wares_controller.rb
+++ app/controllers/manage/course_wares_controller.rb
@@ -6,10 +6,12 @@ class Manage::CourseWaresController < ApplicationController
   end
   
   def index
+    authorize! :manage, CourseWare
     @chapter = Chapter.find(params[:chapter_id])
   end
 
   def new
+    authorize! :manage, CourseWare
     @chapter = Chapter.find(params[:chapter_id])
     @course_ware = @chapter.course_wares.new
 
@@ -17,6 +19,7 @@ class Manage::CourseWaresController < ApplicationController
   end
 
   def create
+    authorize! :manage, CourseWare
     @chapter = Chapter.find(params[:chapter_id])
     @course_ware = @chapter.course_wares.build(params[:course_ware], :as => :upload)
     @course_ware.creator = current_user
@@ -28,12 +31,14 @@ class Manage::CourseWaresController < ApplicationController
 
   def edit
     @course_ware = CourseWare.find(params[:id])
+    authorize! :manage, @course_ware
     @chapter = @course_ware.chapter
     @select_chapters = @chapter.course.chapters
   end
 
   def update
     @course_ware = CourseWare.find(params[:id])
+    authorize! :manage, @course_ware
     @chapter = @course_ware.chapter
     if @course_ware.update_attributes(params[:course_ware], :as => :upload)
       return redirect_to "/manage/chapters/#{@chapter.id}"
@@ -43,6 +48,7 @@ class Manage::CourseWaresController < ApplicationController
 
   def destroy
     @course_ware = CourseWare.find(params[:id])
+    authorize! :manage, @course_ware
     @chapter = @course_ware.chapter
     @course_ware.destroy
     return redirect_to "/manage/chapters/#{@chapter.id}"
@@ -50,6 +56,7 @@ class Manage::CourseWaresController < ApplicationController
 
   def move_up
     @course_ware = CourseWare.find(params[:id])
+    authorize! :manage, @course_ware
     @chapter = @course_ware.chapter
     @course_ware.move_up
     return redirect_to "/manage/chapters/#{@chapter.id}"
@@ -57,6 +64,7 @@ class Manage::CourseWaresController < ApplicationController
 
   def move_down
     @course_ware = CourseWare.find(params[:id])
+    authorize! :manage, @course_ware
     @chapter = @course_ware.chapter
     @course_ware.move_down
     return redirect_to "/manage/chapters/#{@chapter.id}"
@@ -64,6 +72,7 @@ class Manage::CourseWaresController < ApplicationController
 
   def do_convert
     @course_ware = CourseWare.find(params[:id])
+    authorize! :manage, @course_ware
     @chapter = @course_ware.chapter
     
     @course_ware.file_entity.do_convert(true)
--- app/controllers/manage/courses_controller.rb
+++ app/controllers/manage/courses_controller.rb
@@ -6,6 +6,7 @@ class Manage::CoursesController < ApplicationController
   end
   
   def index
+    authorize! :manage, Course
     if (query = @query = params[:q]).blank?
       @courses = Course.page(params[:page])
     else
@@ -16,10 +17,12 @@ class Manage::CoursesController < ApplicationController
   end
 
   def new
+    authorize! :manage, Course
     @course = Course.new
   end
 
   def create
+    authorize! :manage, Course
     @course = current_user.courses.build(params[:course])
     if @course.save
       return redirect_to :action => :index
@@ -29,10 +32,12 @@ class Manage::CoursesController < ApplicationController
 
   def edit
     @course = Course.find params[:id]
+    authorize! :manage, @course
   end
 
   def update
     @course = Course.find params[:id]
+    authorize! :manage, @course
     tags = params[:course_tags]
 
     if @course.update_attributes(params[:course]) && 
@@ -44,37 +49,45 @@ class Manage::CoursesController < ApplicationController
 
   def show
     @course = Course.find params[:id]
+    authorize! :manage, @course
     @chapters = @course.chapters
   end
 
   def destroy
     @course = Course.find params[:id]
+    authorize! :manage, @course
     @course.destroy
     redirect_to :action => :index
   end
 
   def download_import_sample
+    authorize! :manage, Course
     send_file Course.get_sample_excel_course, :filename => 'course_sample.xlsx'
   end
 
   def import
+    authorize! :manage, Course
   end
 
   def do_import
+    authorize! :manage, Course
     file = params[:excel_file]
     Course.import(file, current_user)
     redirect_to :action => :index
   end
 
   def import_youku_list
+    authorize! :manage, Course
   end
 
   def import_youku_list_2
+    authorize! :manage, Course
     @url = params[:url]
     @data = YoukuVideoList.new(@url).parse
   end
 
   def do_import_youku_list
+    authorize! :manage, Course
     @url = params[:url]
     Course.import_youku_video_list(@url, current_user)
 
@@ -82,14 +95,17 @@ class Manage::CoursesController < ApplicationController
   end
 
   def import_tudou_list
+    authorize! :manage, Course
   end
 
   def import_tudou_list_2
+    authorize! :manage, Course
     @url = params[:url]
     @data = TudouVideoList.new(@url).parse
   end
 
   def do_import_tudou_list
+    authorize! :manage, Course
     @url = params[:url]
     Course.import_tudou_video_list(@url, current_user)
 
