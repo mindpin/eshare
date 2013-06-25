88796a144c1b812be9b1aeb13bd29a9703b555a7
mykingla <kingla_pei@163.com>
2013-06-25 11:43:06 +0800

    Fix sql_step factory data

--- spec/factories/sql_step.rb
+++ spec/factories/sql_step.rb
@@ -1,11 +1,12 @@
 FactoryGirl.define do
   factory :sql_step do
     course_ware
-    title(:title){|n| "title_#{n}" }
-    desc(:desc){|n| "desc_#{n}" }
-    desc(:hint){|n| "hint_#{n}" }
-    desc(:content){|n| "content_#{n}" }
-    desc(:rule){|n| "rule_#{n}" }
-    desc(:init_code){|n| "init_code_#{n}" }
+
+    sequence(:title){|n| "title_#{n}" }
+    sequence(:desc){|n| "desc_#{n}" }
+    sequence(:hint){|n| "hint_#{n}" }
+    sequence(:content){|n| "content_#{n}" }
+    sequence(:rule){|n| "rule_#{n}" }
+    sequence(:init_code){|n| "init_code_#{n}" }
   end
 end
