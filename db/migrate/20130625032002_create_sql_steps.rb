cc325e3bf9082a08be287c75df901516e80ece3b
mykingla <kingla_pei@163.com>
2013-06-25 11:23:22 +0800

    Add sql_steps migration

---
+++ db/migrate/20130625032002_create_sql_steps.rb
@@ -0,0 +1,16 @@
+class CreateSqlSteps < ActiveRecord::Migration
+  def change
+    create_table :sql_steps do |t|
+      t.integer :course_ware_id
+      t.string  :title     # 标题
+      t.text    :desc      # 描述
+      t.text    :hint      # 提示
+      t.text    :content   # 题目要求
+      t.text    :rule      # 验收规则
+      t.text    :init_code # 初始代码
+      
+      t.timestamps
+    end
+
+  end
+end
