82d185486551286348f10d1fce66ee4b8b073051
mykingla <kingla_pei@163.com>
2013-07-02 13:59:09 +0800

    Add transcation when import

--- app/models/chapter.rb
+++ app/models/chapter.rb
@@ -45,29 +45,32 @@ class Chapter < ActiveRecord::Base
   end
 
   def import_course_ware_form_json(json_data, user)
-    course_ware = course_wares.create(
-      {
-        :creator => user,
-        :title => json_data[:title],
-        :desc => json_data[:desc],
-        :kind => json_data[:kind]
-      }, { :as => :import }
-    )
-
-    json_data[:steps].each do |s|
-      course_ware.javascript_steps.create(
-        :content => s[:content],
-        :rule => s[:rule],
-        :title => s[:title],
-        :desc => s[:desc],
-        :hint => s[:hint],
-        :init_code => s[:init_code]
+    CourseWare.transaction do
+      course_ware = course_wares.create(
+        {
+          :creator => user,
+          :title => json_data[:title],
+          :desc => json_data[:desc],
+          :kind => json_data[:kind]
+        }, { :as => :import }
       )
-    end
 
-    course_ware.refresh_total_count!
+      json_data[:steps].each do |s|
+        course_ware.javascript_steps.create(
+          :content => s[:content],
+          :rule => s[:rule],
+          :title => s[:title],
+          :desc => s[:desc],
+          :hint => s[:hint],
+          :init_code => s[:init_code]
+        )
+      end
+
+      course_ware.refresh_total_count!
+
+      course_ware
+    end
     
-    course_ware
   end
 
 end
