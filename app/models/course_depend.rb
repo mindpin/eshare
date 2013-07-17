d40825f895331820753728cf0abdebc26be5ca10
mykingla <kingla_pei@163.com>
2013-07-17 16:26:31 +0800

    fix course_depend model

--- app/models/course_depend.rb
+++ app/models/course_depend.rb
@@ -2,42 +2,30 @@
 class CourseDepend < ActiveRecord::Base
   attr_accessible :before_course_id, :after_course_id
 
-  belongs_to :before_course, :class_name => 'CourseDepend', :foreign_key => :before_course_id
-  belongs_to :after_course, :class_name => 'CourseDepend', :foreign_key => :after_course_id
+  belongs_to :before_course, :class_name => 'Course', :foreign_key => :before_course_id
+  belongs_to :after_course, :class_name => 'Course', :foreign_key => :after_course_id
 
   validates :before_course_id, :uniqueness => {:scope => :after_course_id}
 
   module CourseMethods
     def self.included(base)
-      base.has_many(:before_courses,
-                    :class_name => 'CourseDepend',
-                    :foreign_key => :before_course_id)
-
-      base.has_many(:after_courses,
-                    :class_name => 'CourseDepend',
-                    :foreign_key => :after_course_id)
+      base.has_many :a_courses, :class_name => 'CourseDepend', :foreign_key => :before_course_id
+      base.has_many :after_courses, :through => :a_courses, :source => :after_course
+      base.has_many :b_courses, :class_name => "CourseDepend", :foreign_key => :after_course_id
+      base.has_many :before_courses, :through => :b_courses, :source => :before_course
 
       base.send :include, InstanceMethods
     end
 
     module InstanceMethods
 
-      def before_courses
-        return [] if before_courses.blank?
-        before_courses.where(:before_course_id => id)
-      end
-
-      def after_courses
-        return [] if after_courses.blank?
-        before_courses.where(:after_course_id => id)
-      end
 
       def add_before_course(course)
-        before_courses.where(:before_course_id => course.id).first_or_create
+        b_courses.where(:before_course_id => course.id).first_or_create
       end
 
       def add_after_course(course)
-        after_courses.where(:after_course_id => course.id).first_or_create
+        a_courses.where(:after_course_id => course.id).first_or_create
       end
 
     end
--- spec/models/course/course_depend_spec.rb
+++ spec/models/course/course_depend_spec.rb
@@ -31,7 +31,7 @@ describe CourseDepend do
   	end
 
   	it "course_3" do
-  		@course_3.before_courses.should =~ [@course_1, @course_2]
+  		@course_3.before_courses.should =~ [@course_2, @course_1]
   	end
 
   	it "course_4" do
@@ -71,17 +71,16 @@ describe CourseDepend do
   	end
 
   	it "course_2" do
-  		@course_2.before_courses.should =~ [@course_3, @course_4]
+  		@course_2.after_courses.should =~ [@course_3, @course_4]
   	end
 
   	it "course_3" do
-  		@course_3.before_courses.should =~ [@course_4]
+  		@course_3.after_courses.should =~ [@course_4]
   	end
 
   	it "course_4" do
-  		@course_4.before_courses.should =~ []
+  		@course_4.after_courses.should =~ []
   	end
-
 
   	describe "add_after_course" do
   		before {
