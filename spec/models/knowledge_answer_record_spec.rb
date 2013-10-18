45c29658e7b984077e3233d2ed5027ece30ce1f0
fushang318 <fushang318@gmail.com>
2013-10-18 11:37:23 +0800

    add user.history_knowledge_questions

--- spec/models/knowledge_answer_record_spec.rb
+++ spec/models/knowledge_answer_record_spec.rb
@@ -107,6 +107,68 @@ describe KnowledgeAnswerRecord do
     end
   end
 
+  describe "用户的做题历史" do
+    before{
+      # 创建五个问题 1 2 3 4 5
+      @knowledge_question_1 = FactoryGirl.create :knowledge_question
+      @knowledge_question_2 = FactoryGirl.create :knowledge_question
+      @knowledge_question_3 = FactoryGirl.create :knowledge_question
+      @knowledge_question_4 = FactoryGirl.create :knowledge_question
+      @knowledge_question_5 = FactoryGirl.create :knowledge_question
+      # 创建两个用户 1 2
+      @user_1 = FactoryGirl.create :user
+      @user_2 = FactoryGirl.create :user
+      # 用户 1 做 1 2 3 1 4
+      Timecop.travel(Time.now - 5.hours) do
+        @knowledge_question_1.increase_correct_count_of_user(@user_1)
+      end
+      Timecop.travel(Time.now - 4.hours) do
+        @knowledge_question_2.increase_correct_count_of_user(@user_1)
+      end
+      Timecop.travel(Time.now - 3.hours) do
+        @knowledge_question_3.increase_correct_count_of_user(@user_1)
+      end
+      Timecop.travel(Time.now - 2.hours) do
+        @knowledge_question_1.increase_correct_count_of_user(@user_1)
+      end
+      Timecop.travel(Time.now - 1.hours) do
+        @knowledge_question_4.increase_correct_count_of_user(@user_1)
+      end
+      # 用户 2 做 1 2 2 1 5
+      Timecop.travel(Time.now - 5.hours) do
+        @knowledge_question_1.increase_correct_count_of_user(@user_2)
+      end
+      Timecop.travel(Time.now - 4.hours) do
+        @knowledge_question_2.increase_correct_count_of_user(@user_2)
+      end
+      Timecop.travel(Time.now - 3.hours) do
+        @knowledge_question_2.increase_correct_count_of_user(@user_2)
+      end
+      Timecop.travel(Time.now - 2.hours) do
+        @knowledge_question_1.increase_correct_count_of_user(@user_2)
+      end
+      Timecop.travel(Time.now - 1.hours) do
+        @knowledge_question_5.increase_correct_count_of_user(@user_2)
+      end
+    }
+
+    it{
+      # 用户 1 的历史 4 1 3 2
+      @user_1.history_knowledge_questions.should == [
+        @knowledge_question_4,
+        @knowledge_question_1,
+        @knowledge_question_3,
+        @knowledge_question_2
+      ]
+      # 用户 2 的历史 5 1 2
+      @user_2.history_knowledge_questions.should == [
+        @knowledge_question_5,
+        @knowledge_question_1,
+        @knowledge_question_2
+      ]
+    }
+  end
+
   
 
 end
